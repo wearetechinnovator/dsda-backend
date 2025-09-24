316d5e61acbabf30014d37808d8602cd5e527dbb
Sourav Bishai <souravbishai58558@gmail.com>
2025-09-24 19:30:55 +0530

    checkin

--- models/hotel.model.js
+++ models/hotel.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const hotelSchema = new mongoose.Schema({
+    hotel_id: String,
     hotel_name: String,
     hotel_category: {
         type: mongoose.Schema.Types.ObjectId,
