909bf6f26967cb6157935bf1f93dc5649ab7c47e
Sourav Bishai <106469372+techCodeSB@users.noreply.github.com>
2026-02-10 12:43:47 +0530

    check timestap when payment status check4

--- controllers/payGateway.controller.js
+++ controllers/payGateway.controller.js
@@ -263,6 +263,10 @@ const paymentStatusCheck = async (req, res) => {
             oneHourCheck = true;
         }
 
+        console.log("inittime", initTime)
+        console.log("oneHourCheck", oneHourCheck)
+        console.log("nowTime", nowTime)
+        
         if (payResponse?.txnStatus === "REQ" || (initTime && oneHourCheck && payResponse?.responseCode === "P0030")) return 'Processing';
 
         else if (payResponse?.txnStatus === "SUC") {
